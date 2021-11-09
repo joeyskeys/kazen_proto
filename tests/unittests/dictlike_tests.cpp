842f756ef6a73d082f10c291dfee08633a19b764
joeyskeys <joey.skeys@gmail.com>
2021-11-09 11:20:37 +0800

    add tests for dictlike objects

---
+++ tests/unittests/dictlike_tests.cpp
@@ -0,0 +1,20 @@
+
+#include <catch2/catch_test_macro.hpp>
+
+#include "core/camera.h"
+
+TEST_CASE("Member location equality", "[single-file]") {
+    Camera cam {
+        Vec3f{7.f, 8.f, 15.f},
+        Vec3f{0.f, 3.f, -20.f},
+        Vec3f{0.f, 1.f, 0.f},
+        1.f,
+        1000.f,
+        60.f,
+        &film
+    };
+
+    REQUIRE(cam.address_of("position") == cam.runtime_address_of("position"));
+    REQUIRE(cam.address_of("lookat") == cam.runtime_address_of("lookat"));
+    REQUIRE(cam.address_of("near") == cam.runtime_address_of("near"));
+}
