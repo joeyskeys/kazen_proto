81925fc297893b981be30aa2c595e07522d681fd
joeyskeys <joey.skeys@gmail.com>
2022-02-16 17:03:28 +0800

    fix typo

--- tests/unittests/serialization_tests.cpp
+++ tests/unittests/serialization_tests.cpp
@@ -8,7 +8,6 @@ using namespace std::string_literals;
 
 TEST_CASE("Member reflect", "[single-file") {
     Camera cam{};
-    Person p{"Joey", 30};
 
     auto fov = hana::at_key(cam, BOOST_HANA_STRING("fov"));
 
