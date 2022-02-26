f3d7ea23b0cbb9167dfe5c9dc4edad5c31551130
joeyskeys <joey.skeys@gmail.com>
2022-02-26 16:06:13 +0800

    draft data struct design for light path

--- src/core/light_path.h
+++ src/core/light_path.h
@@ -1,2 +1,57 @@
 #pragma once
 
+#include <ostream>
+
+#include "base/basic_types.h"
+#include "base/vec.h"
+#include "core/spectrum.h"
+
+class RecContext {
+public:
+    uint pixel_x;
+    uint pixel_y;
+    uint depth;
+
+    bool should_record();
+};
+
+enum EventType {
+    // Special ones
+    Start,
+    MaxDepth,
+    RouletteCut,
+    Background,
+    // Hit
+    Reflection,
+    Transmission,
+    Volume,
+    Emission
+};
+
+class LightPathEvent {
+public:
+    // Type
+    EventType       type;
+
+    // Geometric information
+    Vec3f           event_position;
+    Vec3f           ray_origin;
+    Vec3f           ray_direction;
+
+    // Hit object information
+    uint            hit_geom_id;
+    Vec3f           normal;
+    Vec2f           uv;
+
+    // Light path information
+    RGBSpectrum     throughput;
+    RGBSpectrum     Li;
+
+};
+
+class LightPath {
+public:
+    std::vector<LightPathEvent> path;
+
+    void record(std::ostream& o);
+};
