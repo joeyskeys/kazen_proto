667f93f7a6d8ca4f12c6e8e66757bd2f425dfabf
joeyskeys <joey.skeys@gmail.com>
2022-11-16 14:34:26 +0800

    testing ocio

--- tests/libusage_tests/ocio_tests.cpp
+++ tests/libusage_tests/ocio_tests.cpp
@@ -6,11 +6,13 @@ namespace OCIO = OCIO_NAMESPACE;
 
 TEST_CASE("Color space transform", "transform") {
     auto config = OCIO::GetCurrentConfig();
-    auto processor = config->getProcessor(OCIO::ROLE_COMPOSITING_LOG,
+    auto processor = config->getProcessor(OCIO::ROLE_MATTE_PAINT,
         OCIO::ROLE_SCENE_LINEAR);
     auto cpu_processor = processor->getDefaultCPUProcessor();
 
     float rgb[3] = { 0.39053256, 0.4777603, 0.64934449 };
     cpu_processor->applyRGB(rgb);
-    REQUIRE(rgb[0] == Approx(16.1010133f));
+    REQUIRE(rgb[0] == Approx(0.16129f).epsilon(1e-3));
+    REQUIRE(rgb[1] == Approx(0.19182530109362525f).epsilon(1e-3));
+    REQUIRE(rgb[2] == Approx(0.3542388221f).epsilon(1e-3));
 }
