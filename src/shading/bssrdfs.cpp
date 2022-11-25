aa9dab9fddb81cf858cded750669516888ed0091
joeyskeys <joey.skeys@gmail.com>
2022-11-25 10:32:39 +0800

    add Dipole sample&eval

--- src/shading/bssrdfs.cpp
+++ src/shading/bssrdfs.cpp
@@ -1,1 +1,9 @@
 #include "shading/bssrdfs.h"
+
+RGBSpectrum KpDipole::eval(const void* data, const OSL::ShaderGlobals& sg, BSSRDFSample& sample) {
+
+}
+
+RGBSpectrum KpDipole::sample(const void* data, const OSL::ShaderGlobals& sg, BSSRDFSample& sample, const Vec3f& rand) {
+
+}
