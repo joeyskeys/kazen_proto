fcc40484e0c9e67c88a8d8e42b948529235f44bd
joeyskeys <joey.skeys@gmail.com>
2022-11-29 15:44:01 +0800

    adding bssrdf

--- src/shading/bssrdfs.cpp
+++ src/shading/bssrdfs.cpp
@@ -1,9 +1,51 @@
 
+#include "base/utils.h"
+#include "base/vec.h"
 #include "shading/bssrdfs.h"
 #include "shading/context.h"
 
 static bool find_po(ShadingContext* ctx, bssrdf_profile_sample_func& profle_func, const Vec3f& rand) {
+    auto dipole_params = reinterpret_cast<KpDipoleParams*>(ctx->data);
     const float disk_radius = profle_func(ctx.data, rand[0]);
+
+    if (disk_radius == 0.f)
+        return false;
+
+    if (disk_radius >= dipole_params->max_radius)
+        return false;
+
+    auto phi = constants::two_pi<float>() * rand[1];
+    auto disk_point = Vec3f{disk_radius * std::cos(phi), 0.f, disk_radius * std::sin(phi)};
+
+    // Choose a projection axis
+    Frame frame;
+    if (rand[2] < 0.5f) {
+        frame = Frame(ctx->frame.s, ctx->frame.t, ctx->frame.n);
+    }
+    else if (rand[2] < 0.75f) {
+        frame = Frame(ctx->frame.t, ctx->frame.n, ctx->frame.s);
+    }
+    else {
+        frame = Frame(ctx->frame.n, ctx->frame.s, ctx->frame.t);
+    }
+
+    auto h = std::sqrt(square(dipole_params->max_radius) - square(disk_radius));
+    auto hn = h * frame.n;
+    auto entry_pt = disk_point + hn;
+    auto ray_dir = base::normalize(-hn);
+
+    const static int max_intersection_cnt = 10;
+    int found_intersection = 0;
+    Intersection isects[max_intersection_cnt];
+    auto start_pt = entry_pt;
+    for (int i = 0; i < max_intersection_cnt; i++) {
+        Ray r(start_pt, ray_dir);
+        ctx->accel->intersect(r, isects[i]);
+        start_pt = isects[i].P;
+        ++found_intersection;
+    }
+
+    // Randomly chose one intersection
 }
 
 RGBSpectrum KpDipole::eval(ShadingContext* ctx) {
--- src/shading/context.h
+++ src/shading/context.h
@@ -2,11 +2,13 @@
 
 #include <OSL/oslclosure.h>
 
-#include "base/accel.h"
+#include "base/utils.h"
+#include "core/accel.h"
 
 class ShadingContext {
     void*   data;
     OSL::ShaderGlobals* sg,
     void*   closure_sample;
     Accel*  accel;
+    Frame   frame;
 };
