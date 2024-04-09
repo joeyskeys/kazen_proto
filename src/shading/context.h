47594bdf41a90ec524b43a7d7e177afc9529c83c
joeyskeys <joey.skeys@gmail.com>
2024-04-09 16:14:52 +0800

    add a utility function to generate ptx code for osl shader

--- src/shading/context.h
+++ src/shading/context.h
@@ -10,6 +10,7 @@
 #include "core/intersection.h"
 
 using ShaderMap = std::unordered_map<std::string, OSL::ShaderGroupRef>;
+using PTXMap = std::unordered_map<std::string, std::string>;
 
 struct ShadingEngine {
     OSL::ShadingSystem*     osl_shading_sys;
@@ -28,6 +29,45 @@ struct ShadingEngine {
 
     inline void execute(OSL::ShaderGroupRef shader, OSL::ShaderGlobals& sg) const {
         osl_shading_sys->execute(*osl_shading_ctx, *shader, sg);
+    }
+
+    PTXMap gen_ptx() {
+        PTXMap ptx_map;
+        std::vector<const char*> outputs { "Cout" };
+        std::vector<void*> material_interactive_params;
+        for (const auto&[name, groupref] : *shaders) {
+            std::string group_name, fused_name;
+            osl_shading_sys->getattribute(groupref.get(), "groupname", group_name);
+            osl_shading_sys->getattribute(groupref.get(), "group_fused_name",
+                fused_name);
+            osl_shading_sys->attribute(groupref.get(), "renderer_outputs",
+                OSL::TypeDesc(OSL::TypeDesc::STRING, outputs.size()), outputs.data());
+            osl_shading_sys->optimize_group(groupref.get(), nullptr);
+
+            if (!osl_shading_sys->find_symbol(*groupref.get(), OSL::ustring(outputs[0]))) {
+                throw std::runtime_error(fmt::format("requested output '{}', which wasn't found", outputs[0]));
+            }
+
+            // Retrieve the compiled ShaderGroup PTX
+            std::string osl_ptx;
+            osl_shading_sys->getattribute(groupref.get(), "ptx_compiled_version",
+                OSL::TypeDesc::PTR, &osl_ptx);
+            if (osl_ptx.empty()) {
+                throw std::runtime_error(fmt::format("failed to generate PTX for shadergroup {}",
+                    group_name));
+            }
+
+            // TODO: Save PTX if necessary
+
+            // TODO: Save the params in another place
+            void* interactive_params = nullptr;
+            osl_shading_sys->getattribute(groupref.get(), "device_interactive_params",
+                OSL::TypeDesc::PTR, &interactive_params);
+            
+            ptx_map.emplace(name, std::move(osl_ptx));
+        }
+
+        return ptx_map;
     }
 };
 
